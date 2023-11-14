ad8bcc669b57ff5751caeb66a06a28cc64c07a51
BaptisteWetterwald <baptiste.wetterwald@gmail.com>
2023-11-14 17:55:20 +0100

    Finished step 2

--- src/main/java/ensisa/birds/MainController.java
+++ src/main/java/ensisa/birds/MainController.java
@@ -5,11 +5,19 @@ import ensisa.birds.model.BirdRepository;
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.transformation.FilteredList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.util.function.Predicate;
 
 public class MainController {
     private BirdRepository repository;
@@ -32,7 +40,26 @@ public class MainController {
     private ListView<Bird> birdListView;
     @FXML
     private VBox birdView;
+    @FXML
+    private Button editButton;
+    @FXML
+    private Button deleteButton;
+    private FilteredList<Bird> filteredBirdList;
+    @FXML
+    private void deleteButtonAction(ActionEvent event) {
+        repository.birds.remove(getCurrentBird());
+    }
+    @FXML
+    private void editButtonAction(ActionEvent event) {
+        Node node = (Node) event.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        BirdEditDialog dialog = new BirdEditDialog(stage, getCurrentBird());
+        dialog.showAndWait().ifPresent(bird -> getCurrentBird().copyFrom(bird));
+    }
     private final ObjectProperty<Bird> currentBird;
+
+    @FXML
+    private TextField filterTextField;
 
     public MainController() {
         repository = new BirdRepository();
@@ -41,15 +68,29 @@ public class MainController {
     }
     public void initialize() {
         birdListView.setCellFactory(new BirdCellFactory());
-        birdListView.setItems(repository.birds);
-        currentBirdProperty().bind(birdListView.getSelectionModel().selectedItemProperty());
+        filteredBirdList = new FilteredList<>(repository.birds);
+        birdListView.setItems(filteredBirdList);
+        currentBirdProperty().bind(birdListView.getSelectionModel().
+                selectedItemProperty());
         currentBirdProperty().addListener((observable, oldBird, newBird) -> {
-            // Pour une liaison unidirectionnelle, il n'est pas nécessaire de supprimer
-            // l'ancienne liaison avant d'en créer une nouvelle
+        // Pour une liaison unidirectionnelle, il n'est pas nécessaire de supprimer
+        // l'ancienne liaison avant d'en créer une nouvelle
             if (newBird != null)
                 bind(newBird);
         });
-        birdView.visibleProperty().bind(Bindings.createBooleanBinding(() -> getCurrentBird() != null, currentBirdProperty()) );
+        birdView.visibleProperty().bind(Bindings.createBooleanBinding(
+                () -> getCurrentBird() != null, currentBirdProperty()));
+        editButton.disableProperty().bind(Bindings.createBooleanBinding(
+                () -> getCurrentBird() == null, currentBirdProperty()));
+        deleteButton.disableProperty().bind(Bindings.createBooleanBinding(
+                () -> getCurrentBird() == null, currentBirdProperty()));
+        filterTextField.textProperty().addListener((observable, oldText, newText) -> {
+            Predicate<Bird> filter = bird -> {
+                String f = newText.trim().toLowerCase();
+                return f.isEmpty() || bird.getCommonName().toLowerCase().contains(f);
+            };
+            filteredBirdList.setPredicate(filter);
+        });
     }
 
     public Bird getCurrentBird() {
--- src/main/java/ensisa/birds/model/Bird.java
+++ src/main/java/ensisa/birds/model/Bird.java
@@ -121,5 +121,15 @@ public class Bird {
         this.image.setValue(image);
     }
 
+    public void copyFrom(Bird bird) {
+        setFamily(bird.getFamily());
+        setGenus(bird.getGenus());
+        setSpecie(bird.getSpecie());
+        setCommonName(bird.getCommonName());
+        setLatinName(bird.getLatinName());
+        setDescription(bird.getDescription());
+        setImagePath(bird.getImagePath());
+    }
+
 
 }
